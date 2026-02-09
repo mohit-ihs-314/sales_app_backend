3ebeb6877bd171bb705afc770fe415e83395a95a
Dell <mohit@investorhomesolutions.in>
2026-02-09 17:59:06 +0530

    handle gmail smtp

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,31 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // TLS
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
 
-  await transporter.sendMail({
-    from: `"Investor Home Solutions" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+    await transporter.sendMail({
+      from: `"Investor Home Solutions" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("EMAIL SENT SUCCESSFULLY");
+  } catch (err) {
+    console.log("EMAIL ERROR:", err);
+  }
 };
 
 module.exports = sendEmail;
